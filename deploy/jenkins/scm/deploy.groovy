631bfb12c6b281dedd4502c99105bb93ffda292a
leiyang <leiyang@yj2025.com>
2024-05-29 15:10:20 +0800

    maven打包

--- deploy/jenkins/scm/deploy.groovy
+++ deploy/jenkins/scm/deploy.groovy
@@ -27,7 +27,7 @@ def call(Map config, Map deployment) {
                             env.JAVA_HOME = DEFAULT_JAVA_HOME
                             env.MAVEN_HOME = DEFAULT_MAVEN_HOME
                             env.DEFAULT_JDK_DOCKER_IMAGE = DEFAULT_JDK_DOCKER_IMAGE
-                            echo "22222222222222222222222"
+                            echo "22222222222222222222222${deployment.JDK_DOCKER_IMAGE}"
 //                            if(deployment.JDK_DOCKER_IMAGE != ""){
 //                                env.DEFAULT_JDK_DOCKER_IMAGE = ${deployment.JDK_DOCKER_IMAGE}
 //
