94e94151b935760f1b8461232f9eb85a760f9209
leiyang <leiyang@yj2025.com>
2024-06-04 10:59:10 +0800

    测试

--- deploy/jenkins/scm/deploy.groovy
+++ deploy/jenkins/scm/deploy.groovy
@@ -82,8 +82,8 @@ def call(Map config, Map deployment) {
                             sh "echo 'FROM ${DEFAULT_JDK_DOCKER_IMAGE}\n" +
                                     "VOLUME /tmp\n" +
                                     "ADD ${deployment.FILE} ${deployment.APP_NAME}.jar\n" +
-                                    "ENTRYPOINT [\"java\",\"-Djava.security.egd=file:/dev/./urandom\",\"-jar\",\"/${deployment.APP_NAME}.jar\"]' \n" +
-                                    "> Dockerfile "
+                                    "ENTRYPOINT [\"java\",\"-Djava.security.egd=file:/dev/./urandom\",\"-jar\",\"/${deployment.APP_NAME}.jar\"]\n " +
+                                    "' > Dockerfile "
                             sh "cat ./Dockerfile"
                             docker.withRegistry("http://${HARBOR_SERVER_IP}", 'harbor_admin') {
                                 def dockerImage = docker.build("${IMAGE_NAME}", "-f ./Dockerfile .")
