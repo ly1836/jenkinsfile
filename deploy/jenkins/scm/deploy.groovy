f8af3b2b07ba3f067ef76b1bef872bd4d3a22f41
leiyang <leiyang@yj2025.com>
2024-06-04 13:40:18 +0800

    更新

--- deploy/jenkins/scm/deploy.groovy
+++ deploy/jenkins/scm/deploy.groovy
@@ -93,7 +93,8 @@ def call(Map config, Map deployment) {
                     steps {
                         script {
                             sh "echo 'FROM ${DEFAULT_JDK_DOCKER_IMAGE}\n" +
-                                    "ENV TimeZone=Asia/Shanghai\n" +
+                                    "ENV TZ=Asia/Shanghai\n" +
+                                    "RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone\n" +
                                     "VOLUME /tmp\n" +
                                     "ADD ${deployment.FILE} ${deployment.APP_NAME}.jar\n" +
                                     "ENTRYPOINT [\"java\",\"-Djava.security.egd=file:/dev/./urandom\",\"-jar\",\"/${deployment.APP_NAME}.jar\"]\n " +
