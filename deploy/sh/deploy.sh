d32603643329ffba3033a8a1580e85e4a108f14f
leiyang <leiyang@yj2025.com>
2024-06-05 17:59:35 +0800

    更新

--- deploy/sh/deploy.sh
+++ deploy/sh/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [[ "$(docker images -q ${IMAGE_NAME} 2> /dev/null)" != "" ]];
+if [[ "$(docker images ${IMAGE_NAME})" != "" ]];
   then
     docker rmi ${IMAGE_NAME}
 fi
@@ -9,12 +9,12 @@ fi
 # docker pull ${HARBOR_SERVER_IP}/${IMAGE_NAME};
 docker pull ${IMAGE_NAME}
 
-if [[ "$(docker inspect ${deployment.APP_NAME} 2> /dev/null | grep '"Name": "/${deployment.APP_NAME}"')" != "" ]];
+if [[ "$(docker ps -a | grep '${deployment.APP_NAME}')" != "" ]];
   then
-    docker rename ${deployment.APP_NAME} ${deployment.APP_NAME}_old;
-    docker stop ${deployment.APP_NAME}_old;
+    docker rename ${deployment.APP_NAME} ${deployment.APP_NAME}_old
+    docker stop ${deployment.APP_NAME}_old
 fi
 
-mkdir -p /home/logs/${deployment.APP_NAME};
-docker run -d -p ${deployment.APP_PORT}:${deployment.APP_PORT} -v /etc/localtime:/etc/localtime -v /home/logs/${deployment.APP_NAME}:/logs -e TZ=Asia/Shanghai --name ${deployment.APP_NAME} ${IMAGE_NAME};
-docker rm ${deployment.APP_NAME}_old;
+mkdir -p /home/logs/${deployment.APP_NAME}
+docker run -d -p ${deployment.APP_PORT}:${deployment.APP_PORT} -v /etc/localtime:/etc/localtime -v /home/logs/${deployment.APP_NAME}:/logs -e TZ=Asia/Shanghai --name ${deployment.APP_NAME} ${IMAGE_NAME}
+docker rm ${deployment.APP_NAME}_old
