68785858fdaad7a6a130dc4776828aecff43f90b
leiyang <leiyang@yj2025.com>
2024-06-06 09:06:57 +0800

    更新

--- deploy/sh/deploy.sh
+++ deploy/sh/deploy.sh
@@ -11,14 +11,14 @@ docker pull ${IMAGE_NAME}
 
 if [[ "$(docker ps -a | grep '${deployment.APP_NAME}')" != "" ]];
   then
-    docker rename ${deployment.APP_NAME} ${deployment.APP_NAME}_old
-    docker stop ${deployment.APP_NAME}_old
+    docker rename ${deployment.APP_NAME} ${deployment.APP_NAME}-old
+    docker stop ${deployment.APP_NAME}-old
 fi
 
 mkdir -p /home/logs/${deployment.APP_NAME}
 docker run -d -p ${deployment.APP_PORT}:${deployment.APP_PORT} -v /etc/localtime:/etc/localtime -v /home/logs/${deployment.APP_NAME}:/logs -e TZ=Asia/Shanghai --name ${deployment.APP_NAME} ${IMAGE_NAME}
 
-if [[ "$(docker ps -a | grep '${deployment.APP_NAME}_old')" != "" ]];
+if [[ "$(docker ps -a | grep '${deployment.APP_NAME}-old')" != "" ]];
   then
-    docker rm ${deployment.APP_NAME}_old
+    docker rm ${deployment.APP_NAME}-old
 fi
