a02e5a1ec23a11bd18951bbbbc437d28bc5994c9
leiyang <leiyang@yj2025.com>
2024-06-11 13:47:00 +0800

    更新

--- deploy/sh/deploy.sh
+++ deploy/sh/deploy.sh
@@ -4,8 +4,3 @@ if [[ "$(docker images ${IMAGE_NAME})" != "" ]];
 then
   docker rmi -f ${IMAGE_NAME}
 fi
-
-docker login ${HARBOR_SERVER_IP} -u ${HARBOR_USER_NAME} -p ${HARBOR_PASSWORD}
-# docker pull ${HARBOR_SERVER_IP}/${IMAGE_NAME}
-docker pull ${IMAGE_NAME}
-
