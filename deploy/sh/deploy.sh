daed4b447e1ccba58b7cecfb266bc659754fe262
leiyang <leiyang@yj2025.com>
2024-06-11 14:58:56 +0800

    更新

--- deploy/sh/deploy.sh
+++ deploy/sh/deploy.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
-#if [[ "$(docker images ${IMAGE_NAME})" != "" ]]
-#then
-#  docker rmi -f ${IMAGE_NAME}
-#fi
-
-docker login ${HARBOR_SERVER_IP} -u ${HARBOR_USER_NAME} -p ${HARBOR_PASSWORD}
-# docker pull ${HARBOR_SERVER_IP}/${IMAGE_NAME}
-docker pull ${IMAGE_NAME}
-
+# 1.重命名容器、停止容器
 if [[ "$(docker ps -a | grep ${deployment.APP_NAME})" != "" ]]
 then
   docker rename ${deployment.APP_NAME} ${deployment.APP_NAME}-old
   docker stop ${deployment.APP_NAME}-old
 fi
 
+# 2.删除镜像
+if [[ "$(docker images ${IMAGE_NAME})" != "" ]]
+then
+  docker rmi -f ${IMAGE_NAME}
+fi
+
+# 3.拉取镜像
+docker login ${HARBOR_SERVER_IP} -u ${HARBOR_USER_NAME} -p ${HARBOR_PASSWORD}
+# docker pull ${HARBOR_SERVER_IP}/${IMAGE_NAME}
+docker pull ${IMAGE_NAME}
+
+# 4.启动容器
 mkdir -p /home/logs/${deployment.APP_NAME}
 docker run -d -p ${deployment.APP_PORT}:${deployment.APP_PORT} -v /etc/localtime:/etc/localtime -v /home/logs/${deployment.APP_NAME}:/logs -e TZ=Asia/Shanghai --name ${deployment.APP_NAME} ${IMAGE_NAME}
 
+# 5.删除旧容器
 if [[ "$(docker ps -a | grep ${deployment.APP_NAME}-old)" != "" ]]
 then
   docker rm -f ${deployment.APP_NAME}-old
