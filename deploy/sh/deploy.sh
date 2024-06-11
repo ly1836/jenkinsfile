e0153c8259225f350a110c367bbe9f9990853fa2
leiyang <leiyang@yj2025.com>
2024-06-11 13:49:54 +0800

    更新

--- deploy/sh/deploy.sh
+++ deploy/sh/deploy.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 
-if [[ "$(docker images ${IMAGE_NAME})" != "" ]];
+if [[ "$(docker images ${IMAGE_NAME})" != "" ]]
 then
   docker rmi -f ${IMAGE_NAME}
+fi
+
+docker login ${HARBOR_SERVER_IP} -u ${HARBOR_USER_NAME} -p ${HARBOR_PASSWORD}
+# docker pull ${HARBOR_SERVER_IP}/${IMAGE_NAME}
+docker pull ${IMAGE_NAME}
+
+if [[ "$(docker ps -a | grep ${deployment.APP_NAME})" != "" ]]
+then
+  docker rename ${deployment.APP_NAME} ${deployment.APP_NAME}-old
+  docker stop ${deployment.APP_NAME}-old
+fi
+
+mkdir -p /home/logs/${deployment.APP_NAME}
+docker run -d -p ${deployment.APP_PORT}:${deployment.APP_PORT} -v /etc/localtime:/etc/localtime -v /home/logs/${deployment.APP_NAME}:/logs -e TZ=Asia/Shanghai --name ${deployment.APP_NAME} ${IMAGE_NAME}
+
+if [[ "$(docker ps -a | grep ${deployment.APP_NAME}-old)" != "" ]]
+then
+  docker rm -f ${deployment.APP_NAME}-old
 fi
