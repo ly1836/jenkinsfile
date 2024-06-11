93ad3b9e7fb64cea5ced652a6367c12e648c0c61
leiyang <leiyang@yj2025.com>
2024-06-11 14:50:36 +0800

    更新

--- deploy/sh/deploy.sh
+++ deploy/sh/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [[ "$(docker images ${IMAGE_NAME})" != "" ]]
-then
-  docker rmi -f ${IMAGE_NAME}
-fi
+#if [[ "$(docker images ${IMAGE_NAME})" != "" ]]
+#then
+#  docker rmi -f ${IMAGE_NAME}
+#fi
 
 docker login ${HARBOR_SERVER_IP} -u ${HARBOR_USER_NAME} -p ${HARBOR_PASSWORD}
 # docker pull ${HARBOR_SERVER_IP}/${IMAGE_NAME}
