859e7f8ad69a0027e5541114f568ff6a5215eee6
leiyang <leiyang@yj2025.com>
2024-06-11 13:30:58 +0800

    更新

--- deploy/jenkins/scm/deploy.groovy
+++ deploy/jenkins/scm/deploy.groovy
@@ -144,13 +144,13 @@ def call(Map config, Map deployment) {
                                         '\
                                             pwd; \
                                             rm -f ./deploy.sh; \
+                                            echo ${deploy_sh} > ./deploy.sh; \
                                             
                                             
                                         '\
                                    """
                             }
-//                            echo ${deploy_sh} > ./deploy.sh; \
-//                                            chmod +x ./deploy.sh; \
+                            // chmod +x ./deploy.sh; \
                             //./deploy.sh; \
                         }
                     }
