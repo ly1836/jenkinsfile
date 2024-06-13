1eb9289e44066a4d3865c76db85bd9cf5702f305
leiyang <leiyang@yj2025.com>
2024-06-13 10:40:01 +0800

    测试

--- deploy/jenkins/scm/deploy.groovy
+++ deploy/jenkins/scm/deploy.groovy
@@ -28,6 +28,9 @@ def call(Map config, Map deployment) {
     if (config.TYPE == "jar") {
         pipeline {
             agent any
+            tools {
+                'org.jenkinsci.plugins.docker.commons.tools.DockerTool' '18.09'
+            }
             environment {
                 // 默认镜像
                 DEFAULT_JDK_DOCKER_IMAGE = "openjdk:8"
@@ -37,6 +40,8 @@ def call(Map config, Map deployment) {
                 DEFAULT_MAVEN_HOME = "/usr/local/maven/apache-maven-3.8.1"
                 // 远程服务器IP
                 REMOTE_SERVER_IP = "192.168.1.79"
+                // docker
+                DOCKER_CERT_PATH = credentials('docker-jenkins')
             }
             parameters {
                 // 发布环境
